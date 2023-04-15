e08e15d106802213d0db56149a4613a54755c8d6
KonstantinSTUDENT <121194637+KonstantinSTUDENT@users.noreply.github.com>
2023-04-15 20:18:19 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿Console.Clear();
+        Console.WriteLine("Введите значение массива");
+            string stroka = Console.ReadLine();
+            a = stroka.Split(' ');
+            var result = new string[a .Length];
+            var realSize = 0;
+            foreach (var value in a )
+            {
+                if (value.Length <= 3)
+                {
+                    result[realSize] = value;
+                    realSize++;
+                }
+            }
+            Console.WriteLine(string.Join(Environment.NewLine, result, 0, realSize));
+            Console.ReadKey(true);
